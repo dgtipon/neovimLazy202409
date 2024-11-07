924d128ee86c18bb1eef9a482a831d4c0669add6
dgtipon <dgtipon4577@proton.me>
2024-11-07 11:15:39 -0800

    Modified Tokyonight, then commented out the modifications.

--- lua/tipon/colorschemes/tokyonight.lua
+++ lua/tipon/colorschemes/tokyonight.lua
@@ -13,11 +13,12 @@ return {
 		-- 	local fg_dark = "#B4D0E9"
 		-- 	local fg_gutter = "#627E97"
 		-- 	local border = "#547998"
-
+		-- 	local red = "#ff0000"
+		--
 		-- 	require("tokyonight").setup({
 		-- 		style = "night",
 		-- 		on_colors = function(colors)
-		-- 			colors.bg = bg
+		-- 			colors.bg = red -- bg
 		-- 			colors.bg_dark = bg_dark
 		-- 			colors.bg_float = bg_dark
 		-- 			colors.bg_highlight = bg_highlight
@@ -34,8 +35,7 @@ return {
 		-- 			colors.fg_sidebar = fg_dark
 		-- 		end,
 		-- 	})
-		-- load the colorscheme here
-		-- vim.cmd([[colorscheme tokyonight]])
+		--
 		-- end,
 	},
 }
