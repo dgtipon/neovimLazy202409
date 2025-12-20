0e71d216248c0d7f5cf7eff31d8dac7b5545cf1f
dgtipon <dgtipon4577@proton.me>
2025-12-20 08:20:28 -0800

    Changed <leader> to space.

--- lua/tipon/plugins/alpha.lua
+++ lua/tipon/plugins/alpha.lua
@@ -20,11 +20,11 @@ return {
 		-- Set menu
 		dashboard.section.buttons.val = {
 			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-			dashboard.button(";ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-			dashboard.button(";ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button(";fs", "  > Find String", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button(";sr", "󰁯  > Session Restore For Current Directory", "<cmd>SessionRestore<CR>"),
-			dashboard.button(";sf", "󰁯  > Session Find", "<cmd>SessionSearch<CR>"),
+			dashboard.button("Space ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+			dashboard.button("Space ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("Space fs", "  > Find String", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("Space sr", "󰁯  > Session Restore For Current Directory", "<cmd>SessionRestore<CR>"),
+			dashboard.button("Space sf", "󰁯  > Session Find", "<cmd>SessionSearch<CR>"),
 			dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
 		}
 
