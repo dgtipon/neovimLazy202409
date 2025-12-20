16ede75a35180cae857917e12bf5704de28c378f
dgtipon <dgtipon4577@proton.me>
2025-12-20 08:49:21 -0800

    Added mini-jump.lua to configuration for smart f and t.

---
+++ lua/tipon/plugins/mini-jump.lua
@@ -0,0 +1,23 @@
+return {
+	"echasnovski/mini.nvim",
+	branch = "stable", -- Use stable for reliability
+	config = function()
+		require("mini.jump").setup({
+			-- Module mappings. Use `''` (empty string) to disable one.
+			mappings = {
+				forward = "f",
+				backward = "F",
+				forward_till = "t",
+				backward_till = "T",
+				repeat_jump = ";",
+			},
+			-- Delay values (in ms) for different functionalities. Set any to -1 to disable.
+			delay = {
+				-- Delay between jump and highlighting all possible jumps
+				highlight = 250,
+				-- Delay between jump and automatic stop if idle (no jump is done)
+				idle_stop = 10000000, -- Disable idle stop by default (set to 1500 for clever-f-like timeout)
+			},
+		})
+	end,
+}
